500e633c1a36a5a7f582ebb0ac24ff983b9c2f27
AlejandroLijoi <alejandrolijoi@live.com>
2019-10-19 02:14:44 -0300

    Create base

---
+++ FrameworkBase.Api.Application.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FrameworkBase.Api.Application.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
